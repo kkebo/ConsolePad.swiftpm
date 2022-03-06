18d0f2b66ae991b39bbad4bd14871f7fa798d2d1
Kenta Kubo <kkk669@users.noreply.github.com>
2022-03-06 20:50:10 +0900

    Reduce compile time

--- Sources/ConsolePad/Views/ConsoleView.swift
+++ Sources/ConsolePad/Views/ConsoleView.swift
@@ -22,50 +22,52 @@ extension ConsoleView: View {
         }
         .navigationTitle("Console")
         .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(role: .destructive, action: self.viewModel.clear) {
-                    Image(systemName: "trash")
-                }
-                .foregroundColor(
-                    self.viewModel.messages.isEmpty
-                        ? .secondary.opacity(0.5)
-                        : .red
-                )
-                .disabled(self.viewModel.messages.isEmpty)
-                .keyboardShortcut("K", modifiers: [.command])
+        .toolbar { self.toolbar }
+    }
+
+    @ToolbarContentBuilder private var toolbar: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button(role: .destructive, action: self.viewModel.clear) {
+                Image(systemName: "trash")
             }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Menu {
-                    Picker(
-                        "Log Level",
-                        selection: self.$viewModel.logLevel
-                    ) {
-                        ForEach(LogLevel.allCases) {
-                            if let name = $0.systemImageName {
-                                Label($0.description, systemImage: name)
-                            } else {
-                                Text($0.description)
-                            }
+            .foregroundColor(
+                self.viewModel.messages.isEmpty
+                ? .secondary.opacity(0.5)
+                : .red
+            )
+            .disabled(self.viewModel.messages.isEmpty)
+            .keyboardShortcut("K", modifiers: [.command])
+        }
+        ToolbarItem(placement: .navigationBarTrailing) {
+            Menu {
+                Picker(
+                    "Log Level",
+                    selection: self.$viewModel.logLevel
+                ) {
+                    ForEach(LogLevel.allCases) {
+                        if let name = $0.systemImageName {
+                            Label($0.description, systemImage: name)
+                        } else {
+                            Text($0.description)
                         }
                     }
-                } label: {
-                    if self.viewModel.logLevel == .all {
-                        Label(
-                            self.viewModel.logLevel.description,
-                            systemImage: "line.3.horizontal.decrease.circle"
-                        )
-                    } else {
-                        Label(
-                            self.viewModel.logLevel.description,
-                            systemImage: "line.3.horizontal.decrease.circle.fill"
-                        )
-                    }
                 }
-                .labelStyle(.titleAndIcon)
+            } label: {
+                if self.viewModel.logLevel == .all {
+                    Label(
+                        self.viewModel.logLevel.description,
+                        systemImage: "line.3.horizontal.decrease.circle"
+                    )
+                } else {
+                    Label(
+                        self.viewModel.logLevel.description,
+                        systemImage: "line.3.horizontal.decrease.circle.fill"
+                    )
+                }
             }
+            .labelStyle(.titleAndIcon)
         }
-    }
+    } 
 }
 
 struct ConsoleView_Previews: PreviewProvider {
