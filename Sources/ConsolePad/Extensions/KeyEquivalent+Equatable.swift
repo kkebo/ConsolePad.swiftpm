2ff340598b82fab809e685d1259c2e9b62596c98
Kenta Kubo <kkk669@users.noreply.github.com>
2023-04-17 00:15:32 +0900

    Update .swift-format

--- Sources/ConsolePad/Extensions/KeyEquivalent+Equatable.swift
+++ Sources/ConsolePad/Extensions/KeyEquivalent+Equatable.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 extension KeyEquivalent: Equatable {
+    /// Returns a Boolean value indicating whether two values are equal.
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.character == rhs.character
     }
