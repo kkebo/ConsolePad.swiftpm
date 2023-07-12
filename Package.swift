d7d939fdec428f924521af84405eab123be2f480
Kenta Kubo <kkk669@users.noreply.github.com>
2023-07-13 01:37:36 +0900

    Bump SwiftUI-Introspect to 0.9.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     products: [],
     dependencies: [
-        .package(url: "https://github.com/siteline/SwiftUI-Introspect", "0.9.0"..<"0.10.0")
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect", "0.9.1"..<"0.10.0")
     ],
     targets: [
         .executableTarget(
