91584f595c3bdb6be1cce55ecc97ce1a3d691f68
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-09-07 22:31:00 +0900

    chore: bump version to 1.0 (33)

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
             bundleIdentifier: "xyz.kebo.ConsolePad",
             teamIdentifier: "X4678G5DL2",
             displayVersion: "1.0",
-            bundleVersion: "32",
+            bundleVersion: "33",
             appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.orange),
             supportedDeviceFamilies: [
